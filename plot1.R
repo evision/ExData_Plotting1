123442314ef71067e6bb96a496f6ce2058a25857
Eugene Valencia <eugene.valencia@primefactors.ph>
2015-06-06 22:32:53 +0800

    Update plot1 script and image.

--- plot1.R
+++ plot1.R
@@ -10,11 +10,11 @@ hhpc <- read.delim("household_power_consumption.txt", sep=";", na.strings="?")
 ## Get only the rows we need (2007-02-01 to 2007-02-02)
 df <- subset(hhpc, Date == "1/2/2007" | Date == "2/2/2007")
 
-## Just make sure we have correct plot layout
-par(mfrow = c(1, 1))
-
 ## Export to PNG
 png(file = "ExData_Plotting1/plot1.png", width=480, height=480)
+
+## Just make sure we have correct plot layout
+par(mfrow = c(1, 1))
 
 ## Draw plot 1
 hist(df$Global_active_power, col="red", main="Global Active Power", 
